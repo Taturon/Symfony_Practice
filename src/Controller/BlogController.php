3f3b2f8bc9b8f3f38dccbcbd5e883210fbc66df0
Taturon <taturon5511@gmail.com>
2021-05-23 12:00:10 +0900

    [Add] Blogコントローラーにshowアクションを追加

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -14,4 +14,15 @@ class BlogController extends AbstractController
     {
         // ...
     }
+
+    /**
+     * @Route("/blog/{slug}", name="blog_show")
+     */
+    public function show(string $slug): Response
+    {
+        // $slug will equal the dynamic part of the URL
+        // e.g. at /blog/yay-routing, then $slug='yay-routing'
+
+        // ...
+    }
 }
