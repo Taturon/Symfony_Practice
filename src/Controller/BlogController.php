9a56f6640e40c05ddf0e9e79d1f8b5b54904500c
Taturon <taturon5511@gmail.com>
2021-05-29 16:45:28 +0900

    [Add] Blogコントローラーにindexアクションを追加

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,6 +9,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
+     * @Route("/blog/{page}", name="blog_index", defaults={"page": 1, "title": "Hello world!"})
+     */
+    public function index(int $page, string $title): Response
+    {
+        // ...
+    }
+
+    /**
      * @Route("/blog/{page<\d+>?1}", name="blog_list", priority=2)
      */
     public function list(int $page): Response
