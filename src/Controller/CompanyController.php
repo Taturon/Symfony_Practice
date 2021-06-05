0eee579263c28a560c128523bbecf06df0521bc5
Taturon <taturon5511@gmail.com>
2021-06-05 14:55:12 +0900

    [Add] Companyコントローラーを追加

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/CompanyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompanyController extends AbstractController
+{
+    /**
+     * @Route({
+     *     "en": "/about-us",
+     *     "nl": "/over-ons"
+     * }, name="about_us")
+     */
+    public function about(): Response
+    {
+        // ...
+    }
+}
