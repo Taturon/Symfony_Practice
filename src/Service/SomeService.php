189dac12f84c8e7eb14b76d82582172f5045521f
Taturon <taturon5511@gmail.com>
2021-06-06 11:55:25 +0900

    [Add] Someサービスを追加

---
+++ src/Service/SomeService.php
@@ -0,0 +1,20 @@
+<?php
+// src/Service/SomeService.php
+namespace App\Service;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class SomeService
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function someMethod()
+    {
+        // ...
+    }
+}
