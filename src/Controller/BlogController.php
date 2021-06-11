74df7287e0b6daa811bcea0ab3a25a090aca65f6
Taturon <taturon5511@gmail.com>
2021-06-12 08:37:17 +0900

    [Delete] Blogコントローラーindexアクションからエラーハンドリングの例を削除

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 /**
@@ -37,11 +36,6 @@ class BlogController extends AbstractController
      */
     public function index(int $page, string $title): Response
     {
-        $product = ...;
-        if (!$product) {
-            throw $this->createNotFoundException('The product does not exist');
-        }
-        return $this->render(...);
     }
 
     /**
