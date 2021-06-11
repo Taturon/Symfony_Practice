3e68a68c5ba87c5a4578625e6d2d6d6ba57effb4
Taturon <taturon5511@gmail.com>
2021-06-12 07:22:15 +0900

    [Add] Luckyコントローラーnumberメソッドにてログを取る処理を追加

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,6 +2,7 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,9 +13,11 @@ class LuckyController extends AbstractController
     /**
      * @Route("/lucky/number/{max}", name="app_lucky_number")
      */
-    public function number(int $max): Response
+    public function number(int $max, LoggerInterface $logger): Response
     {
         $number = random_int(0, $max);
+
+        $logger->info('We are logging!');
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
