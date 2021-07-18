545a28009d8430b0e176df85af449f660c378ddf
Taturon <taturon5511@gmail.com>
2021-07-18 15:38:00 +0900

    [Add] swiftmailer:spool:sendコマンドをコントローラーで使用する例を追加

---
+++ src/Controller/SpoolController.php
@@ -0,0 +1,32 @@
+<?php
+// src/Controller/SpoolController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class SpoolController extends AbstractController
+{
+    public function sendSpool(int $messages = 10, KernelInterface $kernel): Response
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'swiftmailer:spool:send',
+            'fooArgument' => 'barValue',
+            '--message-limit' => $messages,
+        ]);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $content = $output->fetch();
+
+        return new Response($content);
+    }
+}
