93e95d166559e86c3af2e8e99d60118cf140a1a6
Taturon <taturon5511@gmail.com>
2021-07-19 21:28:45 +0900

    [Add] カラフルなHTMLを生成する例を追加

--- src/Controller/SpoolController.php
+++ src/Controller/SpoolController.php
@@ -2,10 +2,12 @@
 // src/Controller/SpoolController.php
 namespace App\Controller;
 
+use SensioLabs\AnsiConverter\AnsiToHtmlConverter;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -22,11 +24,16 @@ class SpoolController extends AbstractController
             '--message-limit' => $messages,
         ]);
 
-        $output = new BufferedOutput();
+        $output = new BufferedOutput(
+            OutputInterface::VERBOSITY_NORMAL,
+            true
+        );
         $application->run($input, $output);
 
+        $converter = new AnsiToHtmlConverter();
         $content = $output->fetch();
 
-        return new Response($content);
+        return new Response($converter->convert($content));
+
     }
 }
