513e1fcc30d18a67e1e8f76be80443f0610b4051
Taturon <taturon5511@gmail.com>
2021-07-24 10:10:02 +0900

    [Add] イテレーションのコマンドオプションの設定例を追加

--- src/Command/GreetCommand.php
+++ src/Command/GreetCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GreetCommand extends Command
@@ -16,6 +17,13 @@ class GreetCommand extends Command
                 'names',
                 InputArgument::IS_ARRAY | InputArgument::REQUIRED,
                 'Who do you want to greet (separate multiple names with a space)?'
+            )
+            ->addOption(
+                'iterations',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'How many times should the message be printed?',
+                1
             )
         ;
     }
