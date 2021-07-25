3040a85a06ae90a13aecccc856388776ba2b0141
Taturon <taturon5511@gmail.com>
2021-07-25 12:13:59 +0900

    [Add] lockメソッドによって同じサーバー上で同じコマンドが複数回実行されることを防ぐ例を追加

---
+++ src/Command/UpdateContentsCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateContentsCommand extends Command
+{
+    use LockableTrait;
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if (!$this->lock()) {
+            $output->writeln('The command is already running in another process.');
+
+            return Command::SUCCESS;
+        }
+
+        $this->release();
+
+        return Command::SUCCESS;
+    }
+}
