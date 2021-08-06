59b5c9c02feb5d4cd14acfb7e5474c27951d22a4
Taturon <taturon5511@gmail.com>
2021-08-07 06:28:08 +0900

    [Add] 単に情報をフィードバックするinfoヘルパーの使用例を追加

--- src/Command/GreetCommand.php
+++ src/Command/GreetCommand.php
@@ -119,6 +119,7 @@ class GreetCommand extends Command
             'Lorem ipsum dolor sit amet',
             'Consectetur adipiscing elit',
         ]);
+        $io->info('Lorem ipsum dolor sit amet');
 
         $names = $input->getArgument('names');
         if (count($names) > 0) {
