c47d68fe05d6fc37eb1d8fd8aff159956bbee385
Taturon <taturon5511@gmail.com>
2021-08-08 08:18:32 +0900

    [Add] 独自のメッセージスタイルを定義するCustomStyleを追加

---
+++ src/Console/CustomStyle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Console;
+
+use Symfony\Component\Console\Style\StyleInterface;
+
+class CustomStyle implements StyleInterface
+{
+    // ...implement the methods of the interface
+}
