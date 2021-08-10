70563152da3b27c5ae6b0a6747d56adb2c3a5457
Taturon <taturon5511@gmail.com>
2021-08-11 07:49:52 +0900

    [Add] コマンド実行時にユーザーに確認を求める方法を追加

---
+++ src/Command/YourCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class YourCommand extends Command
+{
+    // ...
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('Continue with this action?', false);
+
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+    }
+}
