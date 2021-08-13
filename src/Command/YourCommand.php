a85718c4b29c736072b91628f8bdc2b5fe38f5a1
Taturon <taturon5511@gmail.com>
2021-08-14 08:43:50 +0900

    [Modify] オートコンプリートを動的に行うように修正

--- src/Command/YourCommand.php
+++ src/Command/YourCommand.php
@@ -14,9 +14,20 @@ class YourCommand extends Command
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $helper = $this->getHelper('question');
-        $bundles = ['AcmeDemoBundle', 'AcmeBlogBundle', 'AcmeStoreBundle'];
-        $question = new Question('Please enter the name of a bundle', 'FooBundle');
-        $question->setAutocompleterValues($bundles);
-        $bundleName = $helper->ask($input, $output, $question);
+
+        $callback = function (string $userInput): array {
+            $inputPath = preg_replace('%(/|^)[^/]*$%', '$1', $userInput);
+            $inputPath = '' === $inputPath ? '.' : $inputPath;
+            $foundFilesAndDirs = @scandir($inputPath) ?: [];
+
+            return array_map(function ($dirOrFile) use ($inputPath) {
+                return $inputPath.$dirOrFile;
+            }, $foundFilesAndDirs);
+        };
+
+        $question = new Question('Please provide the full path of a file to parse');
+        $question->setAutocompleterCallback($callback);
+
+        $filePath = $helper->ask($input, $output, $question);
     }
 }
