6fb31434cdb04faeda8364727bbb7c1122017a33
Taturon <taturon5511@gmail.com>
2021-08-15 10:39:18 +0900

    [Add] formatterヘルパーによって文字列をセクション表示させる例を追加

--- src/Command/YourCommand.php
+++ src/Command/YourCommand.php
@@ -5,39 +5,16 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
-use Symfony\Component\Console\Helper\HelperSet;
-use Symfony\Component\Console\Helper\QuestionHelper;
-use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Validation;
 
 class YourCommand extends Command
 {
-    // ...
-
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $helper = $this->getHelper('question');
-        $question = new Question('Please enter your password');
-        $question->setValidator(function ($value) {
-            if (trim($value) == '') {
-                throw new \Exception('The password cannot be empty');
-            }
-
-            return $value;
-        });
-        $question->setHidden(true);
-        $question->setMaxAttempts(20);
-        $password = $helper->ask($input, $output, $question);
-    }
-
-    public function testExecute()
-    {
-        $commandTester = new CommandTester($command);
-        $commandTester->setInputs(['Test']);
-        $commandTester->setInputs(['This', 'That']);
-        $commandTester->setInputs(['yes']);
-        $commandTester->execute(['command' => $command->getName()]);
+        $formatter = $this->getHelper('formatter');
+        $formattedLine = $formatter->formatSection(
+            'SomeSection',
+            'Here is some message related to that section'
+        );
+        $output->writeln($formattedLine);
     }
 }
