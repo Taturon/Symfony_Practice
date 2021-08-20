a01389a7cb6b1aed743e033c489c6e8e748bc9a1
Taturon <taturon5511@gmail.com>
2021-08-20 14:17:04 +0900

    [Add] プログレスバーの設定方法を追加

--- src/Command/YourCommand.php
+++ src/Command/YourCommand.php
@@ -5,14 +5,18 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class YourCommand extends Command
 {
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $formatter = $this->getHelper('formatter');
-        $message = "This is a very long message, which should be truncated";
-        $truncatedMessage = $formatter->truncate($message, 7, '!!');
-        $output->writeln($truncatedMessage);
+        $progressBar = new ProgressBar($output, 50);
+        $progressBar->start();
+        $i = 0;
+        while ($i++ < 50) {
+            $progressBar->advance();
+        }
+        $progressBar->finish();
     }
 }
