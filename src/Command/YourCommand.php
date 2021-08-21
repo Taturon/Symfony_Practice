e6e2bf94d958d549cb17ae5916ddee28be58c564
Taturon <taturon5511@gmail.com>
2021-08-21 10:22:44 +0900

    [Add] advanceメソッドの引数を指定する例を追加

--- src/Command/YourCommand.php
+++ src/Command/YourCommand.php
@@ -15,7 +15,7 @@ class YourCommand extends Command
         $progressBar->start();
         $i = 0;
         while ($i++ < 50) {
-            $progressBar->advance();
+            $progressBar->advance(3);
         }
         $progressBar->finish();
     }
