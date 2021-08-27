6a5eee93ad287506a3ada312fb0a664427d65d74
Taturon <taturon5511@gmail.com>
2021-08-28 07:58:51 +0900

    [Add] TableSeparatorヘルパーをインポート

--- src/Command/SomeCommand.php
+++ src/Command/SomeCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 // ...
