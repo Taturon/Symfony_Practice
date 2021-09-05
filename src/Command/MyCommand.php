54715c0e580d201808b5b2640ee1ef4f479c3074
Taturon <taturon5511@gmail.com>
2021-09-05 17:21:35 +0900

    [Add] Cursorヘルパーの使用例を追加

--- src/Command/MyCommand.php
+++ src/Command/MyCommand.php
@@ -1,34 +1,18 @@
 <?php
-use Symfony\Component\Console\Output\ConsoleOutputInterface;
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Cursor;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class MyCommand extends Command
 {
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    // ...
+
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
-        if (!$output instanceof ConsoleOutputInterface) {
-            throw new \LogicException('This command accepts only an instance of "ConsoleOutputInterface".');
-        }
-
-        $section1 = $output->section();
-        $section2 = $output->section();
-
-        $section1->writeln('Hello');
-        $section2->writeln('World!');
-        // Output displays "Hello\nWorld!\n"
-
-        // overwrite() replaces all the existing section contents with the given content
-        $section1->overwrite('Goodbye');
-        // Output now displays "Goodbye\nWorld!\n"
-
-        // clear() deletes all the section contents...
-        $section2->clear();
-        // Output now displays "Goodbye\n"
-
-        // ...but you can also delete a given number of lines
-        // (this example deletes the last two lines of the section)
-        $section1->clear(2);
-        // Output is now completely empty!
-
-        return Command::SUCCESS;
+        $cursor = new Cursor($output);
     }
 }
