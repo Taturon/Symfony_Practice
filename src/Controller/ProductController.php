f6f2033e367a8c37df116884b1ba8a2060e6a11b
Taturon <taturon5511@gmail.com>
2021-09-11 08:26:02 +0900

    [Add] Productエンティティを永続化させるcreateProductメソッドを追加

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,6 +9,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
 {
+    /**
+     * @Route("/product", name="create_product")
+     */
+    public function createProduct(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        $entityManager->persist($product);
+
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
     public function index(): Response
     {
         $contents = $this->renderView('product/index.html.twig', [
