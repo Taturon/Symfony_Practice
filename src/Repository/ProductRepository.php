f454e26254191f97d52a9ee4bb4125f56f956826
Taturon <taturon5511@gmail.com>
2021-09-12 08:25:09 +0900

    [Add] 複雑なクエリを実行するfindAllGreaterThanPriceメソッドを追加

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -12,4 +12,22 @@ class ProductRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Product::class);
     }
+
+    /**
+     * @return Product[]
+     */
+    public function findAllGreaterThanPrice(int $price): array
+    {
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT p
+            FROM App\Entity\Product p
+            WHERE p.price > :price
+            ORDER BY p.price ASC'
+        )->setParameter('price', $price);
+
+        // returns an array of Product objects
+        return $query->getResult();
+    }
 }
