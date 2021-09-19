b9317da9f44d7987013e24e457687a3dbca24430
Taturon <taturon5511@gmail.com>
2021-09-19 11:21:29 +0900

    [Add] Taskコントローラーのnewメソッドにユーザーからのリクエストを処理する例を追加

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -20,6 +20,20 @@ class TaskController extends AbstractController
         $task->setDueDate(new \DateTime('tomorrow'));
 
         $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
 
         return $this->render('task/new.html.twig', [
             'form' => $form->createView(),
