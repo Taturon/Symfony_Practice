da02a8e0e8e5b0bbed141387a2ea110fc1eb7be0
Taturon <taturon5511@gmail.com>
2021-09-25 10:34:35 +0900

    [Add] HTTPリクエストの送信方法を指定するsetMethodメソッドの使用例を追加

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -23,6 +23,7 @@ class TaskController extends AbstractController
             'require_due_date' => $dueDateIsRequired,
         ]);
         $form->setAction($this->generateUrl('target_route'));
+        $form->setMethod('GET');
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             // $form->getData() holds the submitted values
