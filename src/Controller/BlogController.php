8e9de1cc9c69a5c33b175af5741974fb8323573f
Taturon <taturon5511@gmail.com>
2021-09-30 07:23:56 +0900

    [Add] AbstractSessionListenerをインポート

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\EventListener\AbstractSessionListener;
 use Symfony\Component\Routing\Annotation\Route;
 
 
