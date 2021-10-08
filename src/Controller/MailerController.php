01742adb4e7a595997357207cf6a743d16b78c2a
Taturon <taturon5511@gmail.com>
2021-10-09 07:47:29 +0900

    [Add] embedFromPathメソッドによって既存のファイルから画像コンテンツを取得する例を追加

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -19,7 +19,7 @@ class MailerController extends AbstractController
             ->getHeaders()
             ->addTextHeader('X-Auto-Response-Suppress', 'OOF, DR, RN, NRN, AutoReply');
             ->attach(fopen('/path/to/documents/contract.doc', 'r'))
-            ->embed(fopen('/path/to/images/logo.png', 'r'), 'logo')
+            ->embedFromPath('/path/to/images/signature.gif', 'footer-signature')
             ->from(Address::create('Fabien Potencier <fabien@example.com>'))
             ->to($toAddresses)
             ->cc('cc@example.com')
