b93253783246b22fb0a4056d72a3b6d5eea984b7
Taturon <taturon5511@gmail.com>
2021-10-10 08:00:25 +0900

    [Add] TemplatedEmailクラスを用いてメールを組み立てる例を追加

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -16,26 +16,17 @@ class MailerController extends AbstractController
      */
     public function sendEmail(MailerInterface $mailer): Response
     {
-        $toAddresses = ['foo@example.com', new Address('bar@example.com')];
-        $email = (new Email())
-            ->getHeaders()
-            ->addTextHeader('X-Auto-Response-Suppress', 'OOF, DR, RN, NRN, AutoReply');
-            ->attach(fopen('/path/to/documents/contract.doc', 'r'))
-            ->embedFromPath('/path/to/images/signature.gif', 'footer-signature')
-            ->html('<img src="cid:logo"> ... <img src="cid:footer-signature"> ...')
-            ->from(Address::create('Fabien Potencier <fabien@example.com>'))
-            ->to($toAddresses)
-            ->cc('cc@example.com')
-            ->addCc('cc2@example.com')
-            ->subject('Time for Symfony Mailer!')
-            ->text(fopen('/path/to/emails/user_signup.txt', 'r'))
-            ->html(fopen('/path/to/emails/user_signup.html', 'r'));
-        try {
-            $mailer->send($email);
-        } catch (TransportExceptionInterface $e) {
-            // some error prevented the email sending; display an
-            // error message or try to resend the message
-        }
+
+        $email = (new TemplatedEmail())
+            ->from('fabien@example.com')
+            ->to(new Address('ryan@example.com'))
+            ->subject('Thanks for signing up!')
+            ->htmlTemplate('emails/signup.html.twig')
+            ->context([
+                'expiration_date' => new \DateTime('+7 days'),
+                'username' => 'foo',
+            ])
+        ;
 
         // ...
     }
