0dc5e5c4f5b46417b699034264c405f3af953687
Taturon <taturon5511@gmail.com>
2021-10-30 08:43:21 +0900

    [Add] MessageSubscriberInterfaceをインポート

--- src/MessageHandler/SmsNotificationHandler.php
+++ src/MessageHandler/SmsNotificationHandler.php
@@ -2,7 +2,9 @@
 // src/MessageHandler/SmsNotificationHandler.php
 namespace App\MessageHandler;
 
+use App\Message\OtherSmsNotification;
 use App\Message\SmsNotification;
+use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class SmsNotificationHandler implements MessageHandlerInterface
