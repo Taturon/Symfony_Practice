d8ef947ff8acde9ac25acff20bf1f03f3eb919b1
Taturon <taturon5511@gmail.com>
2021-11-06 20:31:18 +0900

    [Add] NotificationクラスのgetChannels()メソッドを継承先でオーバーライドする例を追加

---
+++ src/Notifier/InvoiceNotification.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Notifier;
+
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\Recipient\RecipientInterface;
+use Symfony\Component\Notifier\Recipient\SmsRecipientInterface;
+
+class InvoiceNotification extends Notification
+{
+    private $price;
+
+    public function __construct(int $price)
+    {
+        $this->price = $price;
+    }
+
+    public function getChannels(RecipientInterface $recipient)
+    {
+        if (
+            $this->price > 10000
+            && $recipient instanceof SmsRecipientInterface
+        ) {
+            return ['sms'];
+        }
+
+        return ['email'];
+    }
+}
