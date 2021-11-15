2c86d49db04342344707eccc4de28d56cd86530e
Taturon <taturon5511@gmail.com>
2021-11-16 07:32:03 +0900

    [Add] DataExporterクラスを追加

---
+++ src/Service/DataExporter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Stopwatch\Stopwatch;
+
+class DataExporter
+{
+    private $stopwatch;
+
+    public function __construct(Stopwatch $stopwatch)
+    {
+        $this->stopwatch = $stopwatch;
+    }
+
+    public function export()
+    {
+        // the argument is the name of the "profiling event"
+        $this->stopwatch->start('export-data');
+
+        // ...do things to export data...
+
+        // reset the stopwatch to delete all the data measured so far
+        // $this->stopwatch->reset();
+
+        $this->stopwatch->stop('export-data');
+    }
+}
